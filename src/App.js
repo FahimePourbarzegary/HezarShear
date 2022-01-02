1aaed7535249094ebe00d892cc50801ec83122f5
fahime <fahimeh757@gmail.com>
2022-01-02 04:30:32 -0800

    UI First edition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import MainPage from "./components/MainPage.js";
+import FalPage from "./components/FalPage.js";
+import Ashaar from "./components/Ashaar.js";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainPage />
+      <FalPage />
+      <Ashaar />
     </div>
   );
 }
