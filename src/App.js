d9f4344baefd249f5438fb0567e1c2e047dbe9da
fahime <fahimeh757@gmail.com>
2022-01-03 02:14:12 -0800

    Design Change Shaer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from "react";
 import "./App.css";
 import MainPage from "./components/MainPage.js";
 import FalPage from "./components/FalPage.js";
 import Ashaar from "./components/Ashaar.js";
+
 function App() {
   return (
     <div className="App">
